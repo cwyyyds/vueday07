8d48d6ca24466dbef7763803d2b1792470013409
cwyyyds <1013953614@qq.com>
2022-07-09 19:03:40 +0800

    路由第一题

--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import My from '@/views/My'
+import Classification from '@/views/classification'
+import HomePage from '@/views/homePage'
+import Order from '@/views/order'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/homePage',
+  },
+  {
+    path: '/homePage',
+    component: HomePage,
+  },
+  {
+    path: '/order',
+    component: Order,
+  },
+  {
+    path: '/classification',
+    component: Classification,
+  },
+  {
+    path: '/my',
+    component: My,
+  },
+]
+
+const router = new VueRouter({
+  routes,
+})
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: (h) => h(App),
 }).$mount('#app')
